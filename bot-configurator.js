0059168f2ad48549b1a82aa4f8bb8b43626650bc
Piotr Wilczynski <delwing@gmail.com>
2021-10-09 11:48:28 +0200

    extract configuration

--- bot-configurator.js
+++ bot-configurator.js
@@ -1,7 +1,5 @@
 const { Client, version } = require("discord.js");
 const mapCommand = require("./events/map-command");
-const { downloader, retryingDownloader } = require("./providers");
-const { plainRegexp } = require("./extractors");
 
 const major = parseInt(version.substring(0, version.indexOf(".")));
 const eventName = major >= 13 ? "messageCreate" : "message";
@@ -10,13 +8,9 @@ const eventName = major >= 13 ? "messageCreate" : "message";
  *
  * @param {Client} client
  */
-module.exports = (client) => {
+module.exports = (client, config) => {
   client.on(
     eventName,
-    mapCommand({
-      Arkadia: { locationExtractor: plainRegexp("!lok (\\d+)"), provider: retryingDownloader("./internal/map_sync", { retries: 10, delay: 10000 }) },
-      "Arkadia Public": { locationExtractor: plainRegexp("!public (\\d+)"), provider: retryingDownloader("http://arkadia.kamerdyner.net/master3/map_master3.dat1", { retries: 5 }) },
-      Achea: { locationExtractor: plainRegexp("!achea (\\d+)"), provider: downloader("https://raw.githubusercontent.com/IRE-Mudlet-Mapping/AchaeaCrowdmap/gh-pages/Map/map") },
-    })
+    mapCommand(config)
   );
 };
--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const botConfigurator = require("./bot-configurator")
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-botConfigurator(client)
+const { downloader, retryingDownloader } = require("./providers");
+const { plainRegexp } = require("./extractors");
 
-client.login(process.env.BOT_TOKEN)
+let config = {
+  Arkadia: { locationExtractor: plainRegexp("!lok (\\d+)"), provider: require("./internal/kamerdyner") },
+  "Arkadia Public": { locationExtractor: plainRegexp("!public (\\d+)"), provider: retryingDownloader("http://arkadia.kamerdyner.net/master3/map_master3.dat", { retries: 5 }) },
+  Achea: { locationExtractor: plainRegexp("!achea (\\d+)"), provider: downloader("https://raw.githubusercontent.com/IRE-Mudlet-Mapping/AchaeaCrowdmap/gh-pages/Map/map") },
+};
+
+botConfigurator(client, config);
+
+client.login(process.env.BOT_TOKEN);
